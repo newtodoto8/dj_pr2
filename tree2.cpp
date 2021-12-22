f36c192c0761d229786b45ff390072f90ad35f19
newtodoto8 <30366435+newtodoto8@users.noreply.github.com>
2021-12-22 17:20:55 +0530

    Add files via upload

--- tree2.cpp
+++ tree2.cpp
@@ -6,6 +6,8 @@ public:
      int key;
      node* l_child ;
      node* r_child;
+     node* parent;
+
      node()
      {
         ;
@@ -28,19 +30,137 @@ public:
     void insert(int k);
     void inorder(node *root);
     node* successor(node * root);
-    bool nochild(node *root);
+    int nochild(node *root);
     node* predecessor(node * root);
-    node* search
+    node* _search(int val,node *root);
+    int search(int val);
+    void remove(int val);
+
 
 };
 
-bool BST::nochild(node *root)
+void BST:: remove(int val)
+{
+    node* x=_search(val,root);
+    if (nochild(x)==0)
+    {
+        if (x->key>x->parent->key)
+        {
+            x->parent->r_child=NULL;
+        }
+        else
+        {
+            x->parent->l_child=NULL;
+        }
+        delete x;
+    }
+
+    else if (nochild(x)==1)
+    {
+        if (x->key>x->parent->key) //determine if x is r_child or l_child
+        {
+            if (x->l_child==NULL)
+            {
+                x->parent->r_child=x->r_child;
+                x->r_child->parent=x->parent;
+            }
+            else
+            {
+                 x->parent->r_child=x->l_child;
+                 x->l_child->parent=x->parent;
+            }
+            delete x;
+        }
+        else
+        {
+            if (x->l_child==NULL)
+            {
+                x->parent->l_child=x->r_child;
+                x->r_child->parent=x->parent;
+            }
+            else
+            {
+                 x->parent->l_child=x->l_child;
+                 x->l_child->parent=x->parent;
+            }
+            delete x;
+        }
+       
+    }
+    else if (nochild(x)==2)
+    {
+        node* y=_search(successor(x)->key,root);
+        int temp=y->key;
+        remove(y->key);
+        x->key=temp;
+        // cout<<successor(x)->key<<endl;
+        // y->l_child=x->l_child;
+        // y->r_child=x->r_child;
+        // y->parent=x->parent;
+        
+        
+    }
+
+    else
+    {
+        return;
+    }
+    
+}
+
+
+
+int BST::search(int val)
+{
+    node* temp=_search(val,root);
+    if (temp==NULL)
+    {
+        throw std::runtime_error("key not found");
+    }
+    else
+    { return temp->key;}
+}
+
+node* BST:: _search(int val, node *root)
+{
+   if (root==NULL)
+   {
+    return root;
+   } 
+   else if (root->key==val)
+   {
+    return root;
+   }
+   else if (root->key>val)
+   {
+    root=root->l_child;
+    return _search(val,root);
+   }
+   else
+   {
+    root=root->r_child;
+    return _search(val,root);
+   }
+}
+
+int BST::nochild(node *root)
 {
     if (root->l_child==NULL && root->r_child==NULL)
     {
-        return true;
+        return 0;
     }
-    else return false;
+    else if (root->l_child==NULL)
+    {
+        return 1;
+    }
+    else if (root->r_child==NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 2;
+    }
 }
 
 node* BST::predecessor(node *root)
@@ -96,7 +216,7 @@ void BST::insert(int k)
         root->key=k;
         root->l_child=NULL;
         root->r_child=NULL;
-        
+        root->parent=NULL;
     }
     else
     {
@@ -120,6 +240,7 @@ void BST::insert(int k)
             y->l_child->key=k;
             y->l_child->l_child=NULL;
             y->l_child->r_child=NULL;
+            y->l_child->parent=y;
 
 
         }
@@ -129,6 +250,7 @@ void BST::insert(int k)
             y->r_child->key=k;
             y->r_child->l_child=NULL;
             y->r_child->r_child=NULL;
+            y->r_child->parent=y;
         }
 
     }
@@ -139,20 +261,22 @@ BST:: BST()
 }
 void BST::inorder(node *x)
 {
+    // cout<<"inorder traversal of BST"<<endl;
     if (x==NULL)
     { return;}
 
     else
     {
     inorder(x->l_child);
-    cout<<x->key<<endl;
+    cout<<x->key<<" ";
     inorder(x->r_child);
     }
 }
 int main()
 {
     BST a;
-    cout<<"dbvcbasda"<<endl;
+    cout<<endl;
+    
     a.insert(51);
     a.insert(23);
     a.insert(34);
@@ -165,9 +289,21 @@ int main()
     a.insert(113);
     a.insert(54);
     a.insert(41);
+    a.insert(35);
     a.inorder(a.root);
-    cout<<a.successor(a.root)->key<<endl;
-    cout<<a.predecessor(a.root)->key;
+    // cout<<endl<<a.nochild(a._search(54,a.root));
+    cout<<endl;
+    a.remove(34);
+    a.inorder(a.root);
+    cout<<endl;
+    a.remove(35);
+    a.inorder(a.root);
+    
+
+    // a.remove(51);
+    // a.remove(24);
+    // a.inorder(a.root);
+    // 
 
 }
 
